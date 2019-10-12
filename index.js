dff4ccb3b29e514bee6a10cb346ead03c122bcd8
Rand <randomer9@live.co.uk>
2019-10-12 17:52:24 +0100

    Added docummentation

--- index.js
+++ index.js
@@ -7,13 +7,13 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content.toLowerCase().includes("honk")) {
+client.on('message', msg => {               //Bot will react with a honk emote on any message which includes the word 'honk'
+  if (msg.content.toLowerCase().includes("honk")) {   //Case insensitive
     const emoji = msg.guild.emojis.find(emoji => emoji.name === 'honk');
   msg.react(emoji)
       .then(console.log)
       .catch(console.error);
-} else if (msg.content.toLowerCase().includes("hönk")) {
+} else if (msg.content.toLowerCase().includes("hönk")) {  //Support for alternative spelling of 'honk'
     const emoji = msg.guild.emojis.find(emoji => emoji.name === 'honk');
   msg.react(emoji)
       .then(console.log)
@@ -21,7 +21,7 @@ client.on('message', msg => {
 }
 });
 
-client.on('message', msg => {
+client.on('message', msg => {     //The bot will react with a dagger emoji to all messages sent by users specified in config.json
   if (msg.author.id == config.user) {
   msg.react("🗡")
     .then(console.log)
