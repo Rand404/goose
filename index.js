7987a111dcf4827140b948e6d4d3f73d626e71d7
Rand <randomer9@live.co.uk>
2019-10-12 15:25:38 +0100

    Ability for goose to react to the messaes of certain members

---
+++ index.js
@@ -0,0 +1,27 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const auth = require('./auth.json');
+const config = require('./config.json');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase().includes("honk")) {
+    const emoji = msg.guild.emojis.find(emoji => emoji.name === 'honk');
+  msg.react(emoji)
+      .then(console.log)
+      .catch(console.error);
+}
+});
+
+client.on('message', msg => {
+  if (msg.author.id == config.user) {
+  msg.react("🗡")
+    .then(console.log)
+    .catch(console.error);
+} else {}
+});
+
+client.login(auth.token);
