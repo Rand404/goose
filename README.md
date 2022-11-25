0ade276b0d2a2f5ad555d7c666259ccf5ad50b5f
Rand404 <Rand404@users.noreply.github.com>
2022-11-25 22:04:07 +0000

    Update to 2.3 (#43)
    * Updated honk functionality
    
    * Console readouts are now template strings
    
    * Updated LICENSE.md
    
    * messageCreate not message.Create idiot
    
    * Fixed README and set version to 2.3.0

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # goose
+
 <img src="https://img.shields.io/apm/l/vim-mode"> <img src="https://img.shields.io/github/package-json/v/rand404/goose"> <img src="https://img.shields.io/github/last-commit/rand404/goose"></a>
 
 honk honk honk
--- index.js
+++ index.js
@@ -7,35 +7,35 @@ client.once('ready', () => {
   console.log(`HONK HONK HONK ${client.user.tag}!`);
 });
 
-client.on('message', message => {
+client.on('messageCreate', message => {
   const honk = ["honk", "hooonk", "hönk", "hjonk","hjönk", "hoonk"] //Creates a constant where alternative honks can also be specified to account for differences in spelling
   if (message.channel.type == "dm") return; //Rough fix for a bug in which the bot crashes upon being dm'd with a honk
   else if (honk.some(word => message.content.toLowerCase().includes(word)) ) {  //Reacts to any message containing 'honk' or a number of set alternatives with the emoji tied to :honk: - Also makes sure to be case insensitive
-    const emoji = message.guild.emojis.cache.find(emoji => emoji.name === 'honk');
-  message.react(emoji)
-      .then(console.log(colors.blue('Message Honked in:' + message.channel.name)))
+    const reactionEmoji = message.guild.emojis.cache.find(emoji => emoji.name === 'honk');
+  message.react(reactionEmoji)
+      .then(console.log(colors.blue(`Message Honked in: ${message.guild.name} -> ${message.channel.name}`)))
       .catch(console.error);
 } 
 });
 
-client.on('message', message => {     //The bot will react with a dagger emoji when targeting = 10 on a particular message
+client.on('messageCreate', message => {     //The bot will react with a dagger emoji when targeting = 10 on a particular message
   var targeting = Math.floor((Math.random() * 2500)+1);
   if (message.channel.type == "dm") return; 
   else if (targeting == 10) {
-  message.react("🗡")
-    .then(console.log(colors.cyan('DAGGER deployed in:' + message.channel.name)))
+  message.react(dagger)
+    .then(console.log(colors.cyan(`DAGGER deployed in: ${message.guild.name} -> ${message.channel.name}`)))
     .catch(console.error);
 } else {
     console.log(colors.red(targeting + ' Dagger not deployed'));
 }});
 
-client.on('message', message => {   //On any message containing the letter 'h' the bot generates a number between 1 and 1000
+client.on('messageCreate', message => {   //On any message containing the letter 'h' the bot generates a number between 1 and 1000
   if (message.channel.type == "dm") return;
   else if(message.content.toLowerCase().includes("h")) {
     var number = Math.floor((Math.random() * 1000) + 1);
     if (number == 50) { //If that message is a 50 it triggers a special honk
       message.channel.send('HONK')
-        .then(console.log(colors.green('HONK deployed:' + message.channel.name)))
+        .then(console.log(colors.green(`HONK deployed: ${message.guild.name} -> ${message.channel.name}`)))
         .catch(console.error);
   }  else {
       console.log(colors.red(number + ' Honk not deployed')); //On any other number it generates a console log and does nothing
